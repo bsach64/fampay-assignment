37d54b2d3e313927639f4e9ca595e8743ca212a0
Bhavik Sachdev <b.sachdev1904@gmail.com>
2024-12-26 15:31:26 +0530

    ytapi: store YouTube API Keys as env
    API Keys are stored in .env file as a comma separate list.
    The server will try to use all these keys before failing.
    
    Signed-off-by: Bhavik Sachdev <b.sachdev1904@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Could not load environment variables: %v\n", err)
+	}
+
+	// API_KEYS has a list of all valid api keys that can be used
+	apiKeysStr := os.Getenv("API_KEYS")
+	_ = strings.Split(apiKeysStr, ",")
+
 }
